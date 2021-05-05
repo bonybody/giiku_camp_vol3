b20319827a4e68bf01a4af5e180b982c57e87285
mamaredo_d <11kakeru30@gmail.com>
2021-05-05 18:16:50 +0900

    ユメの記憶のヘッダーを追加

---
+++ pages/yume_kioku.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="max-w-content mx-auto">
+    <div class="h-32">
+      <content-header color="slim" :image="image">
+        <template #heading>
+          <span class="text-white">
+            ユメの記憶
+          </span>
+        </template>
+        <template #description>
+          <span class="text-white">
+            ユメの記憶を辿りましょう
+          </span>
+        </template>
+      </content-header>
+    </div>
+  </div>
+</template>
+
+<script>
+import ContentHeader from '@/components/molecules/commons/ContentHeader'
+
+export default {
+  components: { ContentHeader },
+  data () {
+    return {
+      image: {
+        src: require('@/assets/images/menu_icons/yume_kioku.svg'),
+        alt: 'ユメの記憶'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
