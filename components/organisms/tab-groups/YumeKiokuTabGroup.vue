25158edc3f76407e994c17fd21b3b22c7b8c578e
mamaredo_d <11kakeru30@gmail.com>
2021-05-05 22:01:22 +0900

    tabを作成

---
+++ components/organisms/tab-groups/YumeKiokuTabGroup.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="p-4 bg-white rounded-b-frame">
+    <div class="mb-4">
+      <tab-menu v-model="currentTab" :tabs="tabs" :current-tab="currentTab" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TabMenu from '@/components/molecules/tabs/TabMenu'
+
+export default {
+  name: 'YumeKiokuTabGroup',
+  components: { TabMenu },
+  data () {
+    return {
+      currentTab: 0,
+      tabs: [
+        { id: 0, text: 'スベテ' },
+        { id: 1, text: 'ポスト' },
+        { id: 2, text: 'タヨリ' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
