d7c0b286c13ba364fa24e98fe4f45ccb700cd4b0
mamaredo_n <11kakeru30@gmail.com>
2021-05-06 10:45:44 +0900

    ServieSettings componentを作成

---
+++ components/molecules/setting/ServiceSettings.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <app-heading :level="3" size="xl">
+      サービスの設定
+    </app-heading>
+  </div>
+</template>
+
+<script>
+import AppHeading from '@/components/atoms/headings/AppHeading'
+
+export default {
+  name: 'ServiceSettings',
+  components: { AppHeading }
+}
+</script>
+
+<style scoped>
+
+</style>
