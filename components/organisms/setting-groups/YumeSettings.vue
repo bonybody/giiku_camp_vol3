7ab52c51ea8c90cc8b3eb497121a60577b74aeae
mamaredo_d <11kakeru30@gmail.com>
2021-05-06 20:48:19 +0900

    dialogのアニメーションを追加 選択したpreviewItemを削除できる処理を定義

--- components/organisms/setting-groups/YumeSettings.vue
+++ components/organisms/setting-groups/YumeSettings.vue
@@ -13,21 +13,24 @@
       <template v-for="(previewItem, index) in previewItems">
         <div :key="index" class="mt-4">
           <preview-item :title="previewItem.title" :is-myself="previewItem.isMyself">
-            <app-button color="red-500" @click="handleDialog">
+            <app-button color="red-500" @click="showDialog(index)">
               記憶を消す
             </app-button>
           </preview-item>
         </div>
       </template>
     </div>
-    <div v-if="isDialog === true">
+
+    <transition>
       <app-dialog
+        v-show="isDialog"
         :is-close-action="true"
         :title="dialog.title"
         :text="dialog.text"
-        @close-dialog="handleDialog"
+        @click="onDeleteItem"
+        @close-dialog="isDialog = false"
       />
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -42,34 +45,37 @@ export default {
   components: { ServiceSettings, PreviewItem, AppButton, AppHeading, AppDialog },
   data () {
     return {
+      currentNotification: false,
+      selectedItem: null,
       isDialog: false,
       dialog: {
         title: '記憶を消去します',
-        text: 'ユメタヨリの記憶を削除するとユメタヨリを受け取った人の記憶も消去されます'
+        text: 'ユメタヨリの記憶を削除すると<br>' +
+            'ユメタヨリを受け取った人の記憶も<br>' +
+            '消去されます'
       },
-      currentNotification: false,
       previewItems: [
         {
           isMyself: false,
-          title: 'ユメポスト',
+          title: 'ユメポスト0',
           createdAt: '2020/05/05',
           isFavorite: false
         },
         {
           isMyself: false,
-          title: 'ユメポスト',
+          title: 'ユメポスト1',
           createdAt: '2020/05/05',
           isFavorite: true
         },
         {
           isMyself: true,
-          title: 'ユメタヨリ',
+          title: 'ユメタヨリ2',
           createdAt: '2020/05/05',
           isFavorite: false
         },
         {
           isMyself: true,
-          title: 'ユメタヨリ',
+          title: 'ユメタヨリ3',
           createdAt: '2020/05/05',
           isFavorite: true
         }
@@ -87,8 +93,15 @@ export default {
     }
   },
   methods: {
-    handleDialog () {
-      this.isDialog = !this.isDialog
+    showDialog (index) {
+      this.selectedItem = index
+      this.isDialog = true
+    },
+    // async onDeleteItem () {
+    onDeleteItem () {
+      const newPreviewItems = this.previewItems.filter((el, i, self) => el !== self[this.selectedItem])
+      this.previewItems = newPreviewItems
+      this.isDialog = false
     }
   }
   // fetch () {
@@ -102,5 +115,11 @@ export default {
 </script>
 
 <style scoped>
+.v-enter-active, .v-leave-active {
+  transition: opacity 150ms
+}
 
+.v-enter, .v-leave-to {
+  opacity: 0
+}
 </style>
