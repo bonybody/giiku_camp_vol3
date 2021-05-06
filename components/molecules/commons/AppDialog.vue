44ceb3d5853e85943b5bc15c6914be6b36df57d6
mamaredo_d <11kakeru30@gmail.com>
2021-05-06 20:51:27 +0900

    styleの調整

--- components/molecules/commons/AppDialog.vue
+++ components/molecules/commons/AppDialog.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="fixed z-top top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-30">
-    <div class="max-w-content p-4 bg-white rounded-frame shadow-md">
+  <div class="fixed min-h-full box-border px-6 z-top top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
+    <div class="min-w-dialog p-4 bg-white rounded-frame shadow-md">
       <div>
         <app-heading :level="3" size="xl">
           <span class="text-primary">
@@ -9,16 +9,14 @@
         </app-heading>
       </div>
 
-      <div class="text-sm mt-2">
-        {{ text }}
-      </div>
+      <div class="text-sm mt-2 text-gray-400" v-html="text" />
       <div class="flex w-full mt-4">
         <div class="ml-auto">
           <app-button color="primary" @click="$emit('click')">
             確認
           </app-button>
         </div>
-        <div v-if="isCloseAction === true" class="ml-2">
+        <div v-if="isCloseAction === true" class="ml-4">
           <app-button color="gray-400" @click="$emit('close-dialog')">
             キャンセル
           </app-button>
