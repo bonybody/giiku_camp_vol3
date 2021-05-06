17645aa7b188a3ca8c072c002c72b2f4e5679338
mamaredo_n <11kakeru30@gmail.com>
2021-05-06 12:35:45 +0900

    AppToggleButton component を作成

---
+++ components/atoms/forms/AppToggleButton.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="flex items-center w-full">
+    <label for="toggleB" class="flex items-center cursor-pointer">
+      <!-- label -->
+      <slot />
+      <!-- toggle -->
+      <div class="relative">
+        <!-- input -->
+        <input id="toggleB" type="checkbox" class="sr-only" @click="$emit('input', boolState)">
+        <!-- line -->
+        <div :class="getClasses" class="transition-colors block w-14 h-8 rounded-full" />
+        <!-- dot -->
+        <div class="dot bg-white absolute left-1 top-1 w-6 h-6 rounded-full transition shadow-md" />
+      </div>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppToggleButton',
+  props: {
+    boolState: {
+      type: Boolean,
+      default: null,
+      require: true
+    }
+  },
+  computed: {
+    getClasses () {
+      const bgColor = this.boolState ? 'primary' : 'gray-400'
+      return {
+        [`bg-${bgColor}`]: true
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+input:checked ~ .dot {
+  transform: translateX(100%);
+}
+</style>
