4bc5f30d11f57232fd738938c127aa50f839c1e0
mamaredo_n <11kakeru30@gmail.com>
2021-05-06 12:37:32 +0900

    toggle button を追加

--- components/molecules/setting/ServiceSettings.vue
+++ components/molecules/setting/ServiceSettings.vue
@@ -1,17 +1,46 @@
 <template>
   <div>
-    <app-heading :level="3" size="xl">
-      サービスの設定
-    </app-heading>
+    <div>
+      <app-heading :level="3" size="xl">
+        サービスの設定
+      </app-heading>
+    </div>
+    <!-- settingが増えることを見越してlistで作成 -->
+    <!-- <ul><li></li></ul> -->
+    <div class="flex mt-4">
+      <app-toggle-button v-model="current" :bool-state="current">
+        <div class="text-base mr-4">
+          ユメの通知
+        </div>
+      </app-toggle-button>
+    </div>
   </div>
 </template>
 
 <script>
 import AppHeading from '@/components/atoms/headings/AppHeading'
+import AppToggleButton from '@/components/atoms/forms/AppToggleButton'
 
 export default {
   name: 'ServiceSettings',
-  components: { AppHeading }
+  components: { AppHeading, AppToggleButton },
+  props: {
+    currentNotification: {
+      type: Boolean,
+      default: null,
+      require: true
+    }
+  },
+  computed: {
+    current: {
+      get () {
+        return this.currentNotification
+      },
+      set (bool) {
+        this.$emit('input', bool)
+      }
+    }
+  }
 }
 </script>
 
