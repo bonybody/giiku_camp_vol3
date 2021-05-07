edcc6e60fdbafc616fb696392b92d6e4c6fad974
mamaredo_n <11kakeru30@gmail.com>
2021-05-07 21:46:31 +0900

    AppLoading.vueを作成

---
+++ components/atoms/loading/AppLoading.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full h-full">
+    <div class="animate-spin bg-primary w-14 h-14" />
+    now Loading...
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppLoading'
+}
+</script>
+
+<style scoped>
+
+</style>
