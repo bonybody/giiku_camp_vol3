6082abf5705bbb27a5fffa53ac03d11931d21f5f
bonybody <yumaaa312@gmail.com>
2021-05-08 17:32:55 +0900

    api取得時にデータの有無をapi client側に記述

--- components/organisms/tab-groups/YumeKiokuTabGroup.vue
+++ components/organisms/tab-groups/YumeKiokuTabGroup.vue
@@ -7,36 +7,41 @@
       <div v-show="!show">
         <app-loading-animation />
       </div>
-      <template v-for="(previewItem, index) in previewItems">
-        <div :key="index" class="mt-4">
-          <preview-item :is-myself="isMyself(previewItem.type)">
-            <template #title>
-              <nuxt-link :to="`/yume_kioku/` + previewItem.actionId">
-                {{ previewItem.title }}
-              </nuxt-link>
-            </template>
-            <template #detail>
-              <div class="text-right">
-                <button class="w-5 h-5 mb-1 focus:outline-none" @click="changeFavorite(index)">
-                  <svg
-                    :class="(previewItem.isFavorite === true) ? 'text-secondary' : 'text-gray-400'"
-                    class="m-auto fill-current transition-colors"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="12"
-                    height="18"
-                    viewBox="0 0 12 18"
-                  >
-                    <path id="Icon_metro-pin" data-name="Icon metro-pin" d="M18.607,16.485l-5.9-3.469a1.5,1.5,0,0,0-2.069.563,1.557,1.557,0,0,0,.554,2.106l5.9,3.469a1.5,1.5,0,0,0,2.069-.564,1.554,1.554,0,0,0-.553-2.105ZM13.74,12.735l4.59,2.7,1.561-4.423L16.721,9.147,13.74,12.735ZM10.356,24.1l4.391-5.43L12.78,17.51ZM21.787,8.564,17.854,6.251a1.124,1.124,0,0,0-1.551.423,1.167,1.167,0,0,0,.416,1.579l3.934,2.312a1.124,1.124,0,0,0,1.551-.423,1.167,1.167,0,0,0-.416-1.579Z" transform="translate(-10.356 -6.096)" />
-                  </svg>
-                </button>
-                <div class="text-xs text-gray-400">
-                  {{ getJapaneseDate(previewItem.createdAt) }}
-                </div>
-              </div>
-            </template>
-          </preview-item>
+      <div v-show="show">
+        <div v-show="isEmpty">
+          hoge
         </div>
-      </template>
+        <template v-for="(previewItem, index) in previewItems">
+          <div :key="index" class="mt-4">
+            <preview-item :is-myself="isMyself(previewItem.type)">
+              <template #title>
+                <nuxt-link :to="`/yume_kioku/` + previewItem.actionId">
+                  {{ previewItem.title }}
+                </nuxt-link>
+              </template>
+              <template #detail>
+                <div class="text-right">
+                  <button class="w-5 h-5 mb-1 focus:outline-none" @click="changeFavorite(index)">
+                    <svg
+                      :class="(previewItem.isFavorite === true) ? 'text-secondary' : 'text-gray-400'"
+                      class="m-auto fill-current transition-colors"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="12"
+                      height="18"
+                      viewBox="0 0 12 18"
+                    >
+                      <path id="Icon_metro-pin" data-name="Icon metro-pin" d="M18.607,16.485l-5.9-3.469a1.5,1.5,0,0,0-2.069.563,1.557,1.557,0,0,0,.554,2.106l5.9,3.469a1.5,1.5,0,0,0,2.069-.564,1.554,1.554,0,0,0-.553-2.105ZM13.74,12.735l4.59,2.7,1.561-4.423L16.721,9.147,13.74,12.735ZM10.356,24.1l4.391-5.43L12.78,17.51ZM21.787,8.564,17.854,6.251a1.124,1.124,0,0,0-1.551.423,1.167,1.167,0,0,0,.416,1.579l3.934,2.312a1.124,1.124,0,0,0,1.551-.423,1.167,1.167,0,0,0-.416-1.579Z" transform="translate(-10.356 -6.096)" />
+                    </svg>
+                  </button>
+                  <div class="text-xs text-gray-400">
+                    {{ getJapaneseDate(previewItem.createdAt) }}
+                  </div>
+                </div>
+              </template>
+            </preview-item>
+          </div>
+        </template>
+      </div>
     </div>
   </div>
 </template>
@@ -53,6 +58,8 @@ export default {
   data () {
     return {
       previewItems: [],
+      isEmpty: false,
+      itemsNone: false,
       currentTab: 0,
       tabs: [
         { id: 0, text: 'スベテ' },
@@ -68,7 +75,8 @@ export default {
   },
   async fetch () {
     this.show = false
-    let data = []
+
+    let data = false
     try {
       const user = this.$auth.getUser({ doc: true })
       switch (this.currentTab) {
@@ -82,8 +90,11 @@ export default {
           data = await this.$api.yume.getYumeTayoriGroupByUser(user)
           break
       }
-      this.show = true
+      if (!data) {
+        this.isEmpty = true
+      }
       this.previewItems = data
+      this.show = true
     } catch (e) {
       console.error(e)
     }
@@ -95,8 +106,8 @@ export default {
       },
       set (tab) {
         this.currentTab = tab
+        this.isEmpty = false
         // タブが切り替わったらユメ一覧を初期化
-        this.previewItems = []
         // fetchをやり直し
         this.$fetch()
       }
